a6c66154317b2ab4cbd195a3706985ada98ebe6f
Rohit9-bit <tihor9650@gmail.com>
2026-01-24 19:14:55 +0530

    minor change

--- Server/src/app.js
+++ Server/src/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 // CORS policy
 app.use(cors({
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
