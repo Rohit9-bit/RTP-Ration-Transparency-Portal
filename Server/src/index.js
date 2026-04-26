6b87a01b978da2cdb33ee9aa1f6d89042d4751ed
Rohit9-bit <tihor9650@gmail.com>
2026-04-26 20:16:11 +0530

    fix: minor change

--- Server/src/index.js
+++ Server/src/index.js
@@ -23,31 +23,31 @@ import { generateMonthlyQuota } from "./utils/generateMonthlyQuota.js";
 import { generateMonthlyShopStockLedger } from "./utils/generateMonthlyShopStockLedger.js";
 import { getBeneficiaryEntitlements } from "./utils/getBeneficiaryEntitlement.js";
 // Schedule the job to run at 00:05 (5 minutes past midnight) on the 1st day of every month
-cron.schedule(
-  "* * * * *",
-  async () => {
-    console.log("Running monthly quota and stock-ledger generation job...");
-    try {
-      await generateMonthlyQuota().catch((error) => {
-        throw new Error(error);
-      });
-      await generateMonthlyShopStockLedger().catch((error) => {
-        throw new Error(error);
-      });
-      await getBeneficiaryEntitlement().catch((error) => {
-        throw new Error(error);
-      });
+// cron.schedule(
+//   "* * * * *",
+//   async () => {
+//     console.log("Running monthly quota and stock-ledger generation job...");
+//     try {
+//       await generateMonthlyQuota().catch((error) => {
+//         throw new Error(error);
+//       });
+//       await generateMonthlyShopStockLedger().catch((error) => {
+//         throw new Error(error);
+//       });
+//       await getBeneficiaryEntitlement().catch((error) => {
+//         throw new Error(error);
+//       });
 
-      console.log("Job completed successfully.");
-    } catch (error) {
-      console.error(
-        "Error in monthly quota and stock-ledger generation job:",
-        error,
-      );
-    }
-  },
-  {
-    scheduled: true,
-    timezone: "Asia/Kolkata", // Or your relevant timezone
-  },
-);
+//       console.log("Job completed successfully.");
+//     } catch (error) {
+//       console.error(
+//         "Error in monthly quota and stock-ledger generation job:",
+//         error,
+//       );
+//     }
+//   },
+//   {
+//     scheduled: true,
+//     timezone: "Asia/Kolkata", // Or your relevant timezone
+//   },
+// );
